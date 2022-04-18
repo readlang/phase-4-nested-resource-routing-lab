c83fb59e6809c88a3b3b11e8b0cd47b660307b71
Read Langworthy <rlangworthy@gmail.com>
2022-04-18 16:06:00 -0700

    commit tree 15b6f069300c0754ebad0920a2e29b54b4cb26f4

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,42 @@
 class ItemsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+
 
   def index
-    items = Item.all
-    render json: items, include: :user
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+      render json: items
+    else
+      items = Item.all
+      render json: items, include: :user  
+    end
+    
   end
+
+  def show
+    user = User.find(params[:user_id])
+    items = user.items.find(params[:id])
+    render json: items
+  end
+
+  def create
+    user = User.find(params[:user_id])
+    item = user.items.create(item_params)
+    render json: item, status: :created
+
+  end
+
+  private
+
+  def item_params
+    params.permit(:name, :description, :price)
+  end
+
+  def render_not_found_response
+    render json: {error: "User not found"}, status: :not_found
+  end
+
 
 end
